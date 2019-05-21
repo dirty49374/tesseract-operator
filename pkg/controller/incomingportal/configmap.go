bb33d2d047649d1b9de6fb331d7485460a11e487
dirtycode <dirty49374@gmail.com>
2019-05-21 10:12:57 +0900

    update

--- pkg/controller/incomingportal/configmap.go
+++ pkg/controller/incomingportal/configmap.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 
-	tesseractv1alpha1 "github.com/dirty49374/tesseract-operator/pkg/apis/tesseract/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -22,34 +21,11 @@ func (r *ReconcileIncomingPortal) newConfigMapForCR() (*corev1.ConfigMap, error)
 	yaml := buf.String()
 	return &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "tesseract",
+			Name:      "configmap",
 			Namespace: tesseractNamespace,
 		},
 		Data: map[string]string{
 			"envoy.yaml": yaml,
 		},
 	}, nil
-}
-
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *tesseractv1alpha1.IncomingPortal) *corev1.Pod {
-	labels := map[string]string{
-		"app": cr.Name,
-	}
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name + "-pod",
-			Namespace: cr.Namespace,
-			Labels:    labels,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "busybox",
-					Command: []string{"sleep", "3600"},
-				},
-			},
-		},
-	}
 }
